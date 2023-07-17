67ba76612f8779f07ab0ae506149cbfba5ae68da
James Calligeros <jcalligeros99@gmail.com>
2023-07-17 22:29:00 +1000

    saturation... sort of
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,6 @@
  * harmonics as if they were really there. We can use this phenomenon to get better
  * bass performance out of crappy speakers.
  *
- * TODO: harmonic distortion
- *
  * Copyright (C) 2023 James Calligeros <jcalligeros99@gmail.com>
  */
 
@@ -40,8 +38,58 @@ struct Ports {
     ceil: InputPort<Control>,
     amt: InputPort<Control>,
     bypass: InputPort<Control>,
-    //harms: InputPort<Control>,
+    saturation: InputPort<Control>,
+    blend: InputPort<Control>,
+    harmonics: InputPort<Control>
 }
+
+/*
+ * Distortion/saturation module
+ *
+ * Built like this to avoid refactoring when we do this properly
+ */
+struct Distortion {
+    gain: f32,
+    blend: f32,
+    harmonics: f32
+}
+
+trait Saturator {
+    fn new() -> Self;
+    fn update_params(&mut self, ports: &Ports);
+    fn process(&mut self, sample: f32) -> f32;
+}
+
+impl Saturator for Distortion {
+    fn new() -> Self {
+        Self {
+            gain: 0f32,
+            blend: 0f32, // TODO
+            harmonics: 0f32 // TODO
+        }
+    }
+
+    fn update_params(&mut self, ports: &Ports) {
+        if self.gain != *ports.saturation {
+            self.gain = *ports.saturation;
+        }
+
+        if self.blend != *ports.blend {
+            self.blend = *ports.blend;
+        }
+
+        if self.harmonics != *ports.harmonics {
+            self.harmonics = *ports.harmonics
+        }
+
+        // TODO: other params
+    }
+
+    fn process(&mut self, sample: f32) -> f32 {
+        (sample * self.gain).tanh() // TODO: make this good
+    }
+}
+
 
 /*
  * Plugin state
@@ -59,13 +107,13 @@ struct Ports {
  */
 #[uri("https://chadmed.au/bankstown")]
 struct Subwoofer {
-    low_pass: Vec<DirectForm2Transposed::<f32>>,
-    high_pass: Vec<DirectForm2Transposed::<f32>>,
     floor_curr: f32,
     ceil_curr: f32,
     amt_curr: f32,
-    //harms_curr: f32,
-    sample_rate: f32
+    sample_rate: f32,
+    sat: Distortion,
+    low_pass: Vec<DirectForm2Transposed::<f32>>,
+    high_pass: Vec<DirectForm2Transposed::<f32>>
 }
 
 /*
@@ -108,18 +156,19 @@ impl Plugin for Subwoofer {
 
     fn new(info: &PluginInfo, _features: &mut ()) -> Option<Self> {
         Some(Self {
-            low_pass: build_lpfs(20.0, info.sample_rate() as f32),
-            high_pass: build_hpfs(250.0, info.sample_rate() as f32),
             floor_curr: 0f32,
             ceil_curr: 0f32,
             amt_curr: 0f32,
-            //harms_curr: 0f32,
-            sample_rate: info.sample_rate() as f32
+            sample_rate: info.sample_rate() as f32,
+            sat: Saturator::new(),
+            low_pass: build_lpfs(20f32, info.sample_rate() as f32),
+            high_pass: build_hpfs(250f32, info.sample_rate() as f32)
         })
     }
 
     fn run(&mut self, ports: &mut Ports, _features: &mut (), _: u32) {
         BassEx::update_params(self, ports);
+        self.sat.update_params(ports);
         if *ports.bypass == 1.0 {
             for (inl, outl) in Iterator::zip(ports.in_l.iter(), ports.out_l.iter_mut()) {
                 *outl = inl * 1.0;
@@ -130,18 +179,18 @@ impl Plugin for Subwoofer {
         } else { // TODO: implement distortion part of pipeline
             for (inl, outl) in Iterator::zip(ports.in_l.iter(), ports.out_l.iter_mut()) {
                 // Band-pass on the processed sample
-                let processed: f32 = self.high_pass[0].run(self.low_pass[0].run(*inl));
+                let mut processed: f32 = self.low_pass[0].run(*inl);
 
-                // TODO: harmonic distortion
+                processed = self.high_pass[0].run(self.sat.process(processed));
 
                 // Sum back with input signal
                 *outl = (processed * self.amt_curr) + inl;
             }
             for (inr, outr) in Iterator::zip(ports.in_r.iter(), ports.out_r.iter_mut()) {
                 // Band-pass on the processed sample
-                let processed: f32 = self.high_pass[1].run(self.low_pass[1].run(*inr));
+                let mut processed: f32 = self.low_pass[1].run(*inr);
 
-                // TODO: harmonic distortion
+                processed = self.high_pass[1].run(self.sat.process(processed));
 
                 // Sum back with input signal
                 *outr = (processed * self.amt_curr) + inr;
@@ -166,8 +215,6 @@ impl BassEx for Subwoofer {
         if self.amt_curr != *ports.amt {
             self.amt_curr = *ports.amt;
         }
-
-        // TODO: distortion parameters
     }
 }
 
